3a24696e919a6bf16e58d6f066e8d6c05189c365
Suresh <ponsuresh@gmail.com>
2017-04-07 14:40:28 +0530

    test

---
+++ src/com/cts/FileReading.java
@@ -0,0 +1,62 @@
+package com.cts;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.logging.Logger;
+import java.io.IOException;
+
+public class FileReading {
+	
+
+	private static final Logger LOGGER = Logger.getLogger("com.cognizant.ccap.testing.FileReading");
+
+	void bar() throws IOException{
+		try{
+		SampleProject prj=new SampleProject();
+		String zzz1 = "bhuviiii";
+		String fileName = null;
+		prj.bazzz(fileName);
+		secondMethod();
+		if (zzz1 != null) {
+			fileName = zzz1;
+		}
+		}
+		catch(IOException x){
+			LOGGER.info(x.getMessage());
+			x.printStackTrace();
+		}
+	}
+
+	void baz(String fileName) throws IOException{
+		try{
+		// Read the file and write some text into the file.
+					File file = new File(fileName);
+					OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));
+					writer.write("Hello World");
+					writer.close();
+					secondMethod();
+		}
+		catch(IOException x){
+		LOGGER.info(x.getMessage());
+			x.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		String fileName = null;
+
+		if (args != null && args.length > 0) {
+			fileName = args[0];
+		}
+			// Read the file and write some text into the file.
+			File file = new File(fileName);
+			OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));
+			writer.write("Hello World");
+			writer.close();
+	}
+	
+	void secondMethod() throws IOException {
+		throw new ArrayIndexOutOfBoundsException ("zdgff");
+	}
+}
---
+++ src/com/cts/test/InvalidTicketException.java
@@ -0,0 +1,5 @@
+package com.cts.test;
+
+public class InvalidTicketException extends Exception {
+
+}
